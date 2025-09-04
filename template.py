6c323e8f137e3126eff396acbb6b1dad03146f7b
Ayushi Prajapati <ayushiprajapati021@gmail.com>
2025-09-04 15:28:23 +0530

    Data Ingestion

--- template.py
+++ template.py
@@ -7,26 +7,26 @@ logging.basicConfig(level=logging.INFO)
  
 project_name='mlproject'
 
-list_of_file = [
-    # ".github/workflows/.gitkeep",
+list_of_file=[
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_tranier.py",
     f"src/{project_name}/components/model_monitering.py",
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
     f"src/{project_name}/pipelines/prediction_pipeline.py",
-    f"src/{project_name}/pipelines/exception.py",
-    f"src/{project_name}/pipelines/logger.py",
-    f"src/{project_name}/pipelines/utils.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
     "main.py",
     "app.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py"
 ]
+
 
 for filepath in list_of_file:
     filepath = Path(filepath)
